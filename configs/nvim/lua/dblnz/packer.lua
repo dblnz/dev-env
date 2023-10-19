895eeb62fc9480b27d7e794e6d6b699ea6078c31
Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>
2023-10-19 10:45:51 +0300

    Small updates for nvim config

--- configs/nvim/lua/dblnz/packer.lua
+++ configs/nvim/lua/dblnz/packer.lua
@@ -5,37 +5,38 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.4',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-  use( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use( 'nvim-treesitter/playground')
-  use( 'theprimeagen/harpoon' )
-  use( 'mbbill/undotree' )
-  use( 'tpope/vim-fugitive' )
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment these if you want to manage LSP servers from neovim
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    use( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use( 'nvim-treesitter/playground')
+    use( 'theprimeagen/harpoon' )
+    use( 'mbbill/undotree' )
+    use( 'tpope/vim-fugitive' )
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
+      requires = {
+          --- Uncomment these if you want to manage LSP servers from neovim
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
-  }
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'L3MON4D3/LuaSnip'},
+      }
+    }
+    use { 'simrat39/rust-tools.nvim' }
 end)
 
