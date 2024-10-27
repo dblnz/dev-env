6a7b4b1063c2583d7df37226432d34186ece2fda
Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>
2024-10-28 00:15:24 +0200

    Add search by word under cursor command
    Signed-off-by: Doru-Florin Blanzeanu <blanzeanu.doru@protonmail.com>

--- configs/nvim/files/nvim/after/plugin/telescope.lua
+++ configs/nvim/files/nvim/after/plugin/telescope.lua
@@ -6,3 +6,9 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>pw', function()
+	builtin.grep_string({ search = vim.fn.expand("<cword>") });
+end)
+vim.keymap.set('n', '<leader>pW', function()
+	builtin.grep_string({ search = vim.fn.expand("<cWORD>") });
+end)
