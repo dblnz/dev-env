91b63ac14f2231e0cd17680062095e2b6d2cd950
Doru Blânzeanu <dblnz@pm.me>
2025-10-28 22:44:25 +0200

    Add tmux theme
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -35,20 +35,12 @@
 
     # Additional configuration
     extraConfig = ''
-      # Enable 24-bit color support
-      set-option -ga terminal-overrides ",xterm-256color*:Tc"
-
-      # Start pane numbering at 1
-      set -g pane-base-index 1
-
       # Set PATH for tmux sessions
       #set-environment -g PATH "$PATH:/usr/local/bin"
 
       # Better split commands
       bind | split-window -h -c "#{pane_current_path}"
       bind - split-window -v -c "#{pane_current_path}"
-      unbind '"'
-      unbind %
 
       # Vim-like pane navigation
       bind h select-pane -L
@@ -65,19 +57,6 @@
       # Cycle through panes
       bind o select-pane -t :.+
       bind i select-pane -t :.-
-
-      # Quick pane selection
-      bind -r Tab select-pane -t :.+
-
-      # Reload configuration
-      #bind r source-file ~/.config/tmux/tmux.conf \; display "Config reloaded!"
-
-      # Copy mode bindings
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-      # Don't exit copy mode after selection
-      bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-selection-no-clear
     '';
 
     # Plugins
@@ -85,26 +64,7 @@
       tmuxPlugins.better-mouse-mode
       tmuxPlugins.sensible
       tmuxPlugins.yank
-      # {
-      #   plugin = tmuxPlugins.tmux-themepack;
-      #   extraConfig = ''
-      #     set -g @themepack 'powerline/default/cyan'
-      #   '';
-      # }
-      # {
-      #   plugin = tmuxPlugins.resurrect;
-      #   extraConfig = ''
-      #     set -g @resurrect-strategy-nvim 'session'
-      #     set -g @resurrect-capture-pane-contents 'on'
-      #   '';
-      # }
-      # {
-      #   plugin = tmuxPlugins.continuum;
-      #   extraConfig = ''
-      #     set -g @continuum-restore 'on'
-      #     set -g @continuum-save-interval '15'
-      #   '';
-      # }
+      tmuxPlugins.power-theme
     ];
   };
 }
